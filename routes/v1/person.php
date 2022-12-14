a38ea19653782c92e831d73da397e13db18eb703
kajith <kajithaji98@gmail.com>
2022-12-14 15:24:52 +0530

    version1

--- routes/v1/person.php
+++ routes/v1/person.php
@@ -8,4 +8,7 @@ use Illuminate\Support\Facades\Route;
    Route::post('findMobileNumber', 'App\Http\Controllers\version1\Controller\Person\PersonController@findMobileNumber')->name('findMobileNumber');
    Route::post('findEmail', 'App\Http\Controllers\version1\Controller\Person\PersonController@findEmail')->name('findEmail');
    Route::post('storePerson', 'App\Http\Controllers\version1\Controller\Person\PersonController@storePerson')->name('storePerson');
+   Route::get('getSalutation','App\Http\Controllers\version1\Controller\Person\PersonController@getSalutation')->name('getSalutation');
+   Route::get('getCommonData','App\Http\Controllers\version1\Controller\Person\PersonController@getCommonData')->name('getCommonData');
+
 //Searching Part Ended
