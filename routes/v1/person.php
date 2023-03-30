70870111132d17bc290ba9930a7a6e5c903f021e
kajith <kajithaji98@gmail.com>
2023-03-30 14:37:50 +0530

    demo

--- routes/v1/person.php
+++ routes/v1/person.php
@@ -33,3 +33,4 @@ Route::post('addOtherEmail', 'App\Http\Controllers\version1\Controller\Person\Pe
 Route::post('resendOtpForEmail', 'App\Http\Controllers\version1\Controller\Person\PersonController@resendOtpForEmail')->name('resendOtpForEmail');
 Route::post('deleteForEmailByUid', 'App\Http\Controllers\version1\Controller\Person\PersonController@deleteForEmailByUid')->name('deleteForEmailByUid');
 Route::post('emailChangeAsPrimary', 'App\Http\Controllers\version1\Controller\Person\PersonController@emailChangeAsPrimary')->name('emailChangeAsPrimary');
+//user Profile End 
